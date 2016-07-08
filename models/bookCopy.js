c9aa910a00094ed437eec124775fdadcbe1835c0
pswaroopk <swaroopkpydisetty@gmail.com>
2016-07-07 23:49:28 -0500

    added cardNo for bookloan

--- models/bookCopy.js
+++ models/bookCopy.js
@@ -1,10 +1,11 @@
-  /**
+/**
  * @module book
  * @type {object}
  */
 module.exports = {
 
   attributes: {
+    bookid: { type: 'string', required: true, unique: true ,primaryKey:true},
     isbn: { model: 'book', required: true },
     branchid: { model: 'librarybranch', required: true }
   }
--- models/bookLoan.js
+++ models/bookLoan.js
@@ -3,7 +3,6 @@ module.exports = {
   attributes: {
     loanid: { type: 'integer', required: true, primaryKey: true },
     bookcopy: { model: 'bookcopy', required: true },
-    cardno: { model: 'borrower', required: true }
     dateOut: { type: 'date' },
     dateIn: { type: 'date' },
     dueDate: { type: 'date' }
